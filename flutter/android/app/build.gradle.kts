bf3dbb9d9df39d641c91f42c9634fa2e7748945d
ntg2208 <ntg2208@gmail.com>
2026-06-18 20:13:48 +0100

    chore: bump android build version, update web manifest and package.json

--- flutter/android/app/build.gradle.kts
+++ flutter/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "uk.co.homehd.home_hd"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
